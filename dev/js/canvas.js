c55bd6ae7b3e45328f7a5d2b84eda5d9cc54b471
Harry <harryrock67@gmail.com>
2018-11-12 18:16:58 -0500

    Sketch in place
    Added P5 lib and created the canvas

---
+++ dev/js/canvas.js
@@ -0,0 +1,9 @@
+var doodle = document.getElementsByClassName('doodle')[0];
+
+function setup() {
+    var canvas = createCanvas(1500, 600);
+    canvas.parent(doodle);
+    background(10, 10, 10);
+}
+
+function draw() {}
